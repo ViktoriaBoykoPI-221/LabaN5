8cb42b9a193681307bffa2d0526511fd0f0f81f5
Viktoria <V2018006@yandex.ru>
2023-03-19 23:41:30 +0700

    attempt number 6464641

--- ChangingNumbers.cs
+++ ChangingNumbers.cs
@@ -1,12 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace LabaN5
 {
-    internal class Class1
+  internal class ChangingNumbers
+  {
+    public static void EditingNums(string Path)
     {
+      string FirstNumber = "(012) 345-67-89";
+      string Text = string.Empty;
+      using (System.IO.StreamReader Reader = System.IO.File.OpenText(Path))
+      {
+        Text = Reader.ReadToEnd();
+      }
+
+      if (Text.Contains(FirstNumber))
+      {
+        string Pattern = @"\D(\S{4})\D\S*";
+        string Result = "+380 12 345 67 89";
+        Regex regex = new Regex(Pattern);
+        string ResultOfEditing = regex.Replace(FirstNumber, Result);
+        string Fin = Text.Replace(FirstNumber, ResultOfEditing);
+        using (System.IO.StreamWriter File = new System.IO.StreamWriter(Path))
+        {
+          File.Write(Fin);
+        }
+      }
+
+      Console.Clear();
+      if (!Text.Contains(FirstNumber))
+      {
+        Console.Write("Номера для замены не найдены");
+      }
+      else
+      {
+        Console.WriteLine("Замена прошла успешно, нажмите любую клавишу чтобы продолжить");
+      }
+      Console.ReadKey();
     }
+  }
 }
--- Dictionary.cs
+++ Dictionary.cs
@@ -1,12 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
 using System.Text;
-using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text.RegularExpressions;
+using System.Collections;
 
 namespace LabaN5
 {
-    internal class Class1
+  internal class ChangingMistakes
+  {
+    public static void Searching(string Path)
     {
+      var Words = new List<string>()
+      {
+        "ghbdtn", "plhfdcndeqnt", "пирвет", "превед", "привте", "драсьте", "здраствуйте", "сдравствуйте", "сдраствуйте"
+      };
+      var UsingWords = new List<string>(Words);
+
+      for (int IndexOfWord = 0; IndexOfWord < UsingWords.Count; ++IndexOfWord)
+      {
+        if (UsingWords[IndexOfWord] == "ghbdtn" || UsingWords[IndexOfWord] == "пирвет" || UsingWords[IndexOfWord] == "превед" || UsingWords[IndexOfWord] == "привте")
+        {
+          string Word = UsingWords[IndexOfWord];
+          string Text = string.Empty;
+          using (System.IO.StreamReader Reader = System.IO.File.OpenText(Path))
+          {
+            Text = Reader.ReadToEnd();
+          }
+          if (Text.Contains(Word))
+          {
+            string RightWord = "Привет";
+            string Result = Text.Replace(Word, RightWord);
+            using (System.IO.StreamWriter File = new System.IO.StreamWriter(Path))
+            {
+              File.Write(Result);
+            }
+          }
+        }
+        else if (UsingWords[IndexOfWord] == "plhfdcndeqnt" || UsingWords[IndexOfWord] == "драсьте" || UsingWords[IndexOfWord] == "здраствуйте" || UsingWords[IndexOfWord] == "сдравствуйте" || UsingWords[IndexOfWord] == "сдраствуйте")
+        {
+          string Word = UsingWords[IndexOfWord];
+          string Text = string.Empty;
+          using (System.IO.StreamReader Reader = System.IO.File.OpenText(Path))
+          {
+            Text = Reader.ReadToEnd();
+          }
+          if (Text.Contains(Word))
+          {
+            string RightWord = "Здравствуйте";
+            string Result = Text.Replace(Word, RightWord);
+            using (System.IO.StreamWriter File = new System.IO.StreamWriter(Path))
+            {
+              File.Write(Result);
+            }
+          }
+        }
+      }
+      Console.Clear();
+      Console.Write("Замена прошла успешно, нажмите любую клавишу чтобы продолжить.");
+      Console.ReadKey();
     }
+  }
 }
